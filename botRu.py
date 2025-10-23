2707c875f00bfda8408f3afdb39509d2ad980429
BrunoWithoutH <pinheirobrunoevaristo@gmail.com>
2025-10-23 11:25:15 -0300

    Tá funcionando, eu acho

--- botRu.py
+++ botRu.py
@@ -1,78 +1,75 @@
+import sys
 from time import sleep
+from datetime import datetime, date, time, timedelta
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
-user = (input("\nUsuário: "))
-senha = (input("\nSenha: "))
+user = sys.argv[1]
+senha = sys.argv[2]
+moradia = sys.argv[3]
+dias = [d.strip() for d in sys.argv[4].split(',') if d.strip()]
+refeicoes = [r.strip() for r in sys.argv[5].split(',') if r.strip()]
 
+servico = Service(GeckoDriverManager().install())
+navegador = webdriver.Firefox(service=servico)
 
-data = int(input("\nDeseja agendar apartir de que dia dessa semana?: "))
-data = str(data)
+navegador.get("https://ru.fw.iffarroupilha.edu.br/")
 
-servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
+navegador.find_element(By.XPATH, '//*[@id="username"]').send_keys(user)
+navegador.find_element(By.XPATH, '//*[@id="password"]').send_keys(senha)
+navegador.find_element(By.XPATH, '//*[@id="kc-login"]').click()
 
-cont = 1
-navegador.get(
-    "https://ru.fw.iffarroupilha.edu.br/"
-    )
-#sleep(1)
-
-navegador.find_element('xpath','//*[@id="username"]').send_keys(user)
-#sleep(1)
-navegador.find_element('xpath','//*[@id="password"]').send_keys(senha)
-##sleep(1)
-navegador.find_element('xpath','//*[@id="kc-login"]').click()
-#sleep(6)
 home = True
 while home:
     try:
-        navegador.find_element('xpath','//*[@id="frmHeader"]/div[1]')
+        navegador.find_element(By.XPATH, '//*[@id="frmHeader"]/div[1]')
         home = False
     except NoSuchElementException:
         sleep(.1)
 
-navegador.find_element('xpath','//*[@id="frmHeader"]/div[1]').click()
-navegador.find_element(By.XPATH,'//*[@id="j_idt30:j_idt31_j_idt32"]/ul/li[1]/a').click()
+navegador.find_element(By.XPATH, '//*[@id="frmHeader"]/div[1]').click()
+navegador.find_element(By.XPATH, '//*[@id="j_idt30:j_idt31_j_idt32"]/ul/li[1]/a').click()
 sleep(.2)
 
-for i in range(5):
-    navegador.find_element(By.PARTIAL_LINK_TEXT,data).click()
-    sleep(.2)
-    navegador.find_element(By.XPATH,'//*[@id="frmMain:tipoRefeicao_label"]').click()
-    #sleep(.8)
-    navegador.find_element(By.XPATH,'//*[@id="frmMain:tipoRefeicao_1"]').click()
-    #sleep(.8)
-    navegador.find_element(By.XPATH,'//*[@id="frmMain:addButton"]/span[2]').click()
-    #sleep(.8)
-    confirm = True
-    while confirm:
-        try:
-            navegador.find_element(By.XPATH,'//*[@id="frmMain:j_idt79"]/span')
-            confirm = False
-        except NoSuchElementException:
-            sleep(.1)
-    
-    navegador.find_element(By.XPATH,'//*[@id="frmMain:j_idt79"]/span').click()
-    data = int(data) + cont
-    data = str(data)
-sleep(2)
-print("\n---------------------------- Tudo pronto Parça------------------------")
-print("""
-\n
-⠀⠉⠉⠉⣿⡿⠿⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣻⣩⣉⠉⠉
-⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣀⣀⣀⣀⣀⡀⠄⠄⠉⠉⠄⠄⠄
-⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⠄⠄⠄⠄
-⠄⠄⠄⠄⠄⠄⠄⠄⠄⢤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄
-⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠄⠉⠉⠉⣋⠉⠉⠉⠉⠉⠉⠉⠉⠙⠛⢷⡀⠄⠄
-⣿⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠠⣾⣿⣷⣄⣀⣀⣀⣠⣄⣢⣤⣤⣾⣿⡀⠄
-⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣹⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⢟⢁⣠
-⣿⣿⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⣉⣉⣰⣿⣿⣿⣿⣷⣥⡀⠉⢁⡥⠈
-⣿⣿⣿⢹⣇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠒⠛⠛⠋⠉⠉⠛⢻⣿⣿⣷⢀⡭⣤⠄
-⣿⣿⣿⡼⣿⠷⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣠⣿⣟⢷⢾⣊⠄⠄
-⠉⠉⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⣈⣉⣭⣽⡿⠟⢉⢴⣿⡇⣺⣿⣷
-⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠁⠐⢊⣡⣴⣾⣥⣿⣿⣿
-""")
+for data in dias:
+    dia, mes, ano = map(int, data.split('/'))
+    dia_semana = date(ano, mes, dia).weekday()
+    agora = datetime.now()
+    prev_deadline = datetime.combine(date(ano, mes, dia) - timedelta(days=1), time(17, 0))
+    janta_deadline = datetime.combine(date(ano, mes, dia), time(11, 30))
+
+    refeicoes_possiveis = []
+    if moradia == "sim":
+        if agora <= prev_deadline and "cafe" in refeicoes:
+            refeicoes_possiveis.append(1)
+        if agora <= prev_deadline and "almoco" in refeicoes:
+            refeicoes_possiveis.append(2)
+        if agora <= janta_deadline and dia_semana != 4 and "janta" in refeicoes:
+            refeicoes_possiveis.append(3)
+    else:
+        if agora <= prev_deadline:
+            refeicoes_possiveis.append(2)
+
+    for idx in refeicoes_possiveis:
+        navegador.find_element(By.PARTIAL_LINK_TEXT, str(dia)).click()
+        sleep(.2)
+        navegador.find_element(By.XPATH, '//*[@id="frmMain:tipoRefeicao_label"]').click()
+        sleep(.2)
+        navegador.find_element(By.XPATH, f'//*[@id="frmMain:tipoRefeicao_{idx}"]').click()
+        sleep(.2)
+        navegador.find_element(By.XPATH, '//*[@id="frmMain:addButton"]/span[2]').click()
+        confirm = True
+        while confirm:
+            try:
+                navegador.find_element(By.XPATH, '//*[@id="frmMain:j_idt79"]/span')
+                confirm = False
+            except NoSuchElementException:
+                sleep(.1)
+        navegador.find_element(By.XPATH, '//*[@id="frmMain:j_idt79"]/span').click()
+        sleep(.2)
+
+sleep(1)
+print("Agendamentos finalizados.")
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: templates/agendamento.html");
+exit();
+?>
